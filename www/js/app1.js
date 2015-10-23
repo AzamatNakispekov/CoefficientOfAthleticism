f5014418ba59a044528854ec1477904847cbe856
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-10-24 00:13:28 +0600

    приложение для подсчет атлетизма
    приложение для подсчет атлетизма  для КНУ

---
+++ www/js/app1.js
@@ -0,0 +1,13 @@
+function GetAndCount()
+    {
+           var a=23
+           var b=32
+           var c=12
+        
+
+        
+           return (a*b)/c;
+           
+    }
+var test = GetAndCount();
+console.log(test);
