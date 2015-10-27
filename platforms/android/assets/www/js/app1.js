645f7289d1f52058c6d330c2961fd135afc9d9a7
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-10-28 01:41:33 +0600

    Мелкие изменения
    Касаются визулизации

--- platforms/android/assets/www/js/app1.js
+++ platforms/android/assets/www/js/app1.js
@@ -1,13 +1,12 @@
-function GetAndCount()
-    {
-           var a=23
-           var b=32
-           var c=12
-        
 
-        
-           return (a*b)/c;
+function Get()
+    {
+ var a=23;
+ var b=32;
+ var c=1;
+ 
+    return (a*b)/c;
            
     }
-var test = GetAndCount();
+var test = Get();
 console.log(test);
